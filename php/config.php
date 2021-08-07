4dc91d554e0d605d69c6690959dfb0edb248e2e3
Bakare Adebayo <hardebayobakare@gmail.com>
2021-08-07 13:47:44 +0100

    database config

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Database Configuration
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root"); //Database Username
+define("DB_PASSWORD", ""); //Database User Password
+define("DB_DATABASE", ""); //Database Name
+
+?>
